3b8d766a2386c067a046eda73e9eec17917d324e
Chekushka <chizha.dima@gmail.com>
2020-06-04 12:42:47 +0500

    Finish work

--- src/asr_realtime.py
+++ src/asr_realtime.py
@@ -1,54 +1,128 @@
-import cv2 as cv
+import time
+
+import cv2
 import numpy as np
+from tensorflow_core.python.keras.models import load_model
+
+from asr import AwesomeSignRecognizer
 from keyboard import ASRKeyboard
+import copy
 
 ESC_KEY = 27
+SPACE_KEY = 32
+
+# Общие настройки
+prediction = ''
+action = ''
+score = 0
+selected_gesture = ''
 
 # params
-cap_region_x_begin = 0.5  # start point/total width
-cap_region_y_end = 0.8  # start point/total width
+cap_region_x_begin = 0.5
+cap_region_y_end = 0.8
 bg_sub_threshold = 50
+learningRate = 0
+blur_value = 41
+threshold = 60
 
-# vars
-is_bg_captured = False
+thresh = None
+gesture_names = {
+    0: 'H',
+    1: 'E',
+    2: 'L',
+    3: 'O',
+    4: 'W',
+    5: 'R',
+    6: 'D'
+}
+
+# model = load_model('C:\Projects\AwesomeSignRecognition\models\saved_model.hdf5')
+model = load_model('C:\Projects\AwesomeSignRecognition\models\VGG.h5')
 
 def config_keyboard() -> ASRKeyboard:
     return ASRKeyboard(
         bg_capture_keys=[ord('b'), ord('B')],
         bg_reset_keys=[ord('r'), ord('R')],
-        exit_keys=[ESC_KEY]
+        exit_keys=[ESC_KEY],
+        gather_mode_keys=[ord('s'), ord('S')],
+        prediction_mode_keys=[SPACE_KEY]
     )
 
 
+def predict_rgb_image_vgg(image):
+    image = np.array(image, dtype='float32')
+    image /= 255
+    pred_array = model.predict(image)
+    print(f'pred_array: {pred_array}')
+    result = gesture_names[np.argmax(pred_array)]
+    print(f'Result: {result}')
+    print(max(pred_array[0]))
+    score = float("%0.2f" % (max(pred_array[0]) * 100))
+    print(result)
+    return result, score
+
+
 def main():
-    camera = cv.VideoCapture(0)
+    global prediction
+    global score
+    global thresh
+    global action
+
+    camera = cv2.VideoCapture(0)
     asr_keyboard = config_keyboard()
+    asr = AwesomeSignRecognizer()
+    cnt = 0
 
     while camera.isOpened():
-        # Capture frame-by-frame
         ret, frame = camera.read()
 
-        frame = cv.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
-        frame = cv.flip(frame, 1)  # flip the frame horizontally
-        cv.rectangle(frame, (int(cap_region_x_begin * frame.shape[1]), 0),
-                     (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
+        # фильтр сглаживания
+        frame = cv2.bilateralFilter(frame, 5, 50, 100)
+        # Отразим кадр по горизонтали
+        frame = cv2.flip(frame, 1)
+        cv2.rectangle(frame, (int(cap_region_x_begin * frame.shape[1]), 0),
+                      (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
 
-        cv.imshow('original', frame)
+        cv2.imshow('original', frame)
 
-        # if is_bg_captured:
+        if asr.is_bg_captured:
+            img = asr.remove_background(frame)
+            # Вырезаем зону показа руки
+            img = img[0:int(cap_region_y_end * frame.shape[0]),
+                  int(cap_region_x_begin * frame.shape[1]):frame.shape[1]]
+            # Преобразуем изображение в двуцветное
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            blur = cv2.GaussianBlur(gray, (blur_value, blur_value), 0)
+            ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+            # Отрисовываем текст
+            cv2.putText(thresh, f"Prediction: {prediction} ({score}%)", (50, 30), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                        (255, 255, 255))
+            cv2.putText(thresh, f"Action: {action}", (50, 80), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                        (255, 255, 255))
+            cv2.imshow('ori', thresh)
 
-        key = cv.waitKey(10)
+        key = cv2.waitKey(10)
+
         if asr_keyboard.is_exit_key(key):
             break
-        elif asr_keyboard.is_bg_capture_key(key):
-            bg = cv.createBackgroundSubtractorMOG2(0, bg_sub_threshold)
-            is_bg_captured = True
-            print('Background captured')
         elif asr_keyboard.is_bg_reset_key(key):
-            break
+            asr.reset_bg()
+            print('Background reset')
+        elif asr_keyboard.is_bg_capture_key(key):
+            asr.capture_bg()
+            print('Background capture')
+        elif asr_keyboard.is_gather_mode_key(key):
+            asr.gather_mode = not asr.gather_mode
+            print('Gather mode: ' + 'on' if asr.gather_mode else 'off')
+        elif asr_keyboard.is_prediction_mode_key(key):
+            cv2.imshow('original', frame)
+            target = np.stack((thresh,) * 3, axis=-1)
+            target = cv2.resize(target, (224, 224))
+            target = target.reshape(1, 224, 224, 3)
+            prediction, score = predict_rgb_image_vgg(target)
 
     camera.release()
-    cv.destroyAllWindows()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
--- src/keyboard.py
+++ src/keyboard.py
@@ -1,9 +1,10 @@
 class ASRKeyboard:
-    def __init__(self, bg_capture_keys, bg_reset_keys, exit_keys, gather_mode_keys):
+    def __init__(self, bg_capture_keys, bg_reset_keys, exit_keys, gather_mode_keys, prediction_mode_keys):
         self.__bg_capture_keys = bg_capture_keys
         self.__bg_reset_keys = bg_reset_keys
         self.__exit_keys = exit_keys
         self.__gather_mode_keys = gather_mode_keys
+        self.__prediction_mode_keys = prediction_mode_keys
 
     def is_bg_capture_key(self, key: int) -> bool:
         return key in self.__bg_capture_keys
@@ -16,3 +17,6 @@ class ASRKeyboard:
 
     def is_gather_mode_key(self, key: int) -> bool:
         return key in self.__gather_mode_keys
+    
+    def is_prediction_mode_key(self, key: int) -> bool:
+        return key in self.__prediction_mode_keys
